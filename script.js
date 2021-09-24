7abc8de744e5dfbfda6dd199a07811344a08f6af
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-09-23 23:57:50 -0400

    added scripts and fixed html layout

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 var todaysDate = document.getElementById('todaysDate');
 todaysDate.textContent = moment().format("dddd, MMMM Do");
 
+
+
 var checkTime = function () {
 
-     var currentTime = moment().format('H');
+     var currentTime = parseInt(moment(0).format('H'));
 
     var timeBlockElements = $("textarea");
-
+    
     for (var i = 0 ; i < timeBlockElements.length ; i++) {
 
-        var elementID = timeBlockElements[i].id;
+        var elementID = parseInt(timeBlockElements[i].id);
         
         var manipID = document.getElementById(timeBlockElements[i].id)
 
@@ -26,3 +28,11 @@ var checkTime = function () {
 }
 
 setInterval(checkTime(), (1000 * 60) * 5);
+
+$('.saveBtn').on('click', storeValues);
+
+function storeValues() {
+    var input = $(this).siblings('textarea').val().trim();
+    var valueId = $(this).parent().attr('id');
+    localStorage.setItem(valueId, input);
+}
